32bcad835f51257e075e79f601098a5bd9d339c7
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-04-09 22:23:24 +0530

    Oprest

---
+++ plugins/git.js
@@ -0,0 +1,10 @@
+let fetch = require ('node-fetch')
+let handler = async (m, { conn }) => {
+    let devil = `ㅤ
+`.trim()
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/reduced_IMG-20211219-WA0031_2.jpg")).buffer(), devil, 'c💘\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://Instagram.com/kakashi_0.3\n\n\ © follow karle londe⁩' , '❤️', '.❤️', '🙂', ',🙂')
+}
+handler.tags = ['main']
+handler.help = ['git']
+handler.command = /^(git)$/i
+module.exports = handler
